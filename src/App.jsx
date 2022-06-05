eae94e548909f37bd5a7207633e88028cf676f46
zrierc <muhammadfajri.gs@gmail.com>
2022-06-05 16:07:33 +0700

    Change to jsx file, Adding custom class/utility tailwind

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Navs from './components/Navs';
+import Introduction from './components/Introduction';
+
+function App() {
+  return (
+    <div className="App">
+      <Navs />
+
+      <main className="nullish">
+        <Introduction />
+      </main>
+    </div>
+  );
+}
+
+export default App;
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,21 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-    content: [
-      "./src/**/*.{js,jsx,ts,tsx}",
-    ],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
       fontFamily: {
-        'sans': ['Montserrat', 'sans-serif', ...defaultTheme.fontFamily.sans],
-        'mono': ['Oxygen Mono', 'monospace'],
+        sans: ['Montserrat', 'sans-serif', ...defaultTheme.fontFamily.sans],
+        mono: ['Oxygen Mono', 'monospace'],
+      },
+      textUnderlineOffset: {
+        3: '3px',
+      },
+      textDecorationThickness: {
+        1.5: '1.5px',
       },
     },
   },
   plugins: [],
-}
+};
+
